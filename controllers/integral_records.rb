efba2811715d07429b42d0054c32487e80de6045
zhaohongguang <zhaohongguang@yundianjia.com>
2019-01-21 10:03:47 +0800

    打开注释

--- controllers/integral_records.rb
+++ controllers/integral_records.rb
@@ -10,9 +10,9 @@ class ::IntegralRecordsController < ::ApplicationController
   end
 
   def create
-    # rule = Rule.where(id: params[:rule_id]).last
-    # options = { user_id: params[:user_id], rule_id: params[:rule_id], score: rule.score.to_f, note: params[:note] }
-    # IntegralRecord.create_integral_record(options)
+    rule = Rule.where(id: params[:rule_id]).last
+    options = { user_id: params[:user_id], rule_id: params[:rule_id], score: rule.score.to_f, note: params[:note] }
+    IntegralRecord.create_integral_record(options)
     render_json_dump(params)
   end
 end
